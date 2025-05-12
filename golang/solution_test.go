6e8c14b9361ecfcbbafccb82156cd797806e4096
GitHub Action <action@github.com>
2025-05-12 16:32:52 +0000

    test: [20250513] Add (3335)

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problemsKylePade/problemsKylePade_2094"
+	problem "leetCode/problemsKylePade/problemsKylePade_3335"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "2094", "problemsKylePade", problem.Solve)
+	TestEach(t, "3335", "problemsKylePade", problem.Solve)
 }
--- qubhjava/test/TestMain.java
+++ qubhjava/test/TestMain.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 // import problems.problems_3392.Solution;
 // import premiums.premiums_1056.Solution;
 import org.testng.util.Strings;
-import problemsKylePade.problemsKylePade_2094.Solution;
+import problemsKylePade.problemsKylePade_3335.Solution;
 import qubhjava.Testcase;
 
 import java.io.IOException;
@@ -23,7 +23,7 @@ import java.util.List;
 public class TestMain {
 
     private static final Logger log = LoggerFactory.getLogger(TestMain.class);
-    private static final String PROBLEM_ID = "2094";
+    private static final String PROBLEM_ID = "3335";
 
     private Testcase[] loadTestcases() throws IOException {
         String problemFolder = null;
