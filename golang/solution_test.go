11049b11134c61613783f713ec7c233d1664ad7b
GitHub Action <action@github.com>
2025-06-14 16:29:38 +0000

    test: [20250615] Add (1432)

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problemsKylePade/problemsKylePade_2566"
+	problem "leetCode/problemsKylePade/problemsKylePade_1432"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "2566", "problemsKylePade", problem.Solve)
+	TestEach(t, "1432", "problemsKylePade", problem.Solve)
 }
---
+++ problemsKylePade/problemsKylePade_1432/Solution.java
@@ -0,0 +1,18 @@
+package problemsKylePade.problemsKylePade_1432;
+
+import com.alibaba.fastjson.JSON;
+import java.util.*;
+import qubhjava.BaseSolution;
+
+
+public class Solution extends BaseSolution {
+    public int maxDiff(int num) {
+        
+    }
+
+    @Override
+    public Object solve(String[] inputJsonValues) {
+        int num = Integer.parseInt(inputJsonValues[0]);
+        return JSON.toJSON(maxDiff(num));
+    }
+}
