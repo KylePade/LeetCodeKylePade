6279d7bcc096d2405c0e309bde1c858d7e63b196
GitHub Action <action@github.com>
2025-06-18 16:33:39 +0000

    test: [20250619] Add (2294)

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problemsKylePade/problemsKylePade_2966"
+	problem "leetCode/problemsKylePade/problemsKylePade_2294"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "2966", "problemsKylePade", problem.Solve)
+	TestEach(t, "2294", "problemsKylePade", problem.Solve)
 }
