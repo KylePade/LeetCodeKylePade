2857e8b6deb16b1cec53bbef0ad54ceaab122373
GitHub Action <action@github.com>
2025-06-19 16:33:28 +0000

    test: [20250620] Add (3443)

--- qubhjava/test/TestMain.java
+++ qubhjava/test/TestMain.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 // import problems.problems_3392.Solution;
 // import premiums.premiums_1056.Solution;
 import org.testng.util.Strings;
-import problemsKylePade.problemsKylePade_2294.Solution;
+import problemsKylePade.problemsKylePade_3443.Solution;
 import qubhjava.Testcase;
 
 import java.io.IOException;
@@ -23,7 +23,7 @@ import java.util.List;
 public class TestMain {
 
     private static final Logger log = LoggerFactory.getLogger(TestMain.class);
-    private static final String PROBLEM_ID = "2294";
+    private static final String PROBLEM_ID = "3443";
 
     private Testcase[] loadTestcases() throws IOException {
         String problemFolder = null;
