d90931bc22676762fbc8034b0e855fdc401f7aff
GitHub Action <action@github.com>
2025-07-02 16:33:15 +0000

    test: [20250703] Add (3304)

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problemsKylePade/problemsKylePade_3333"
+	problem "leetCode/problemsKylePade/problemsKylePade_3304"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "3333", "problemsKylePade", problem.Solve)
+	TestEach(t, "3304", "problemsKylePade", problem.Solve)
 }
--- rust/test_executor/tests/test.rs
+++ rust/test_executor/tests/test.rs
@@ -1,9 +1,9 @@
 const PROBLEM_FOLDER: &str = "problemsKylePade";
-const PROBLEM_ID: &str = "3333";
+const PROBLEM_ID: &str = "3304";
 
 #[cfg(test)]
 mod test {
-	use solution_3333 as solution;
+	use solution_3304 as solution;
     use test_executor::run_test::run_test;
 
     use crate::{PROBLEM_FOLDER, PROBLEM_ID};
