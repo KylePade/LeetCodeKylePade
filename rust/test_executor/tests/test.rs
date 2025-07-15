0128a07a53cf9e83fc59744a4094f249c6af1d0a
GitHub Action <action@github.com>
2025-07-15 16:35:11 +0000

    test: [20250716] Add (3201)

--- rust/test_executor/tests/test.rs
+++ rust/test_executor/tests/test.rs
@@ -1,9 +1,9 @@
 const PROBLEM_FOLDER: &str = "problemsKylePade";
-const PROBLEM_ID: &str = "3136";
+const PROBLEM_ID: &str = "3201";
 
 #[cfg(test)]
 mod test {
-	use solution_3136 as solution;
+	use solution_3201 as solution;
     use test_executor::run_test::run_test;
 
     use crate::{PROBLEM_FOLDER, PROBLEM_ID};
