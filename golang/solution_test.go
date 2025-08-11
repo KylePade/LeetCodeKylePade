bb8ef8452b0970a796f6fad328492d772067da89
GitHub Action <action@github.com>
2025-08-11 16:35:45 +0000

    test: [20250812] Add (2787)

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problemsKylePade/problemsKylePade_2438"
+	problem "leetCode/problemsKylePade/problemsKylePade_2787"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "2438", "problemsKylePade", problem.Solve)
+	TestEach(t, "2787", "problemsKylePade", problem.Solve)
 }
--- typescript/test.ts
+++ typescript/test.ts
@@ -6,7 +6,7 @@ var _ = require('lodash-contrib');
 const vm = require('node:vm');
 import {CompareResults} from "./common";
 
-const PROBLEM_ID: string = "2438";
+const PROBLEM_ID: string = "2787";
 
 describe("TestMain===" + PROBLEM_ID, () => {
     dotenv.config();
