de0b8faf82215c5c256e97990d338c0c344e377b
GitHub Action <action@github.com>
2025-08-12 16:34:35 +0000

    test: [20250813] Add (326)

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problemsKylePade/problemsKylePade_2787"
+	problem "leetCode/problemsKylePade/problemsKylePade_326"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "2787", "problemsKylePade", problem.Solve)
+	TestEach(t, "326", "problemsKylePade", problem.Solve)
 }
---
+++ problemsKylePade/problemsKylePade_326/Solution.java
@@ -0,0 +1,18 @@
+package problemsKylePade.problemsKylePade_326;
+
+import com.alibaba.fastjson.JSON;
+import java.util.*;
+import qubhjava.BaseSolution;
+
+
+public class Solution extends BaseSolution {
+    public boolean isPowerOfThree(int n) {
+        
+    }
+
+    @Override
+    public Object solve(String[] inputJsonValues) {
+        int n = Integer.parseInt(inputJsonValues[0]);
+        return JSON.toJSON(isPowerOfThree(n));
+    }
+}
